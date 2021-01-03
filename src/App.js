4a66c1cca41bb40bb13a5a233cacc8641bec5122
JihenBenAttia <benattiajihen@gmail.com>
2021-01-03 17:35:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Components/Home'
+import imageInSrc from './imageInSrc.jpg'
+
+function App() {
+  return (
+<div>
+
+
+<div style={{border:"solid 1px black",maxWidth:"100vw" }} >
+
+<h1 className="title red">Your name here</h1>
+
+<br />
+
+<img src={imageInSrc}/>
+
+<br />
+
+<img src="imageInPublic.jpg"/>
+
+</div>
+
+<video width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4"/>
+
+</video>
+</div>
+  );
+}
+
+export default App;
